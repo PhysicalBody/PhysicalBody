c16d3033378d0e528de33e3c4a4523af512ba691
PhysicalBody <102663188+PhysicalBody@users.noreply.github.com>
2022-06-02 08:21:58 -0300

    Delete scripts directory

--- scripts/back-to-top.js
+++
@@ -1,9 +0,0 @@
-
-let toTop = document.querySelector(".toTop");
-window.addEventListener("scroll",function() {
-    if (window.pageYOffset > 600) {
-        toTop.classList.add("active_toTop");
-    } else {
-        toTop.classList.remove("active_toTop");
-   }
-});
